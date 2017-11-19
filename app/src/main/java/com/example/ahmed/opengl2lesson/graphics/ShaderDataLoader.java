21353b10c0d11395a98eec46dadb939426b95c13
shakram02 <ahmedhamdyau@gmail.com>
2017-11-19 10:50:28 +0200

    comments

--- app/src/main/java/com/example/ahmed/opengl2lesson/graphics/ShaderDataLoader.java
+++ app/src/main/java/com/example/ahmed/opengl2lesson/graphics/ShaderDataLoader.java
@@ -7,11 +7,18 @@ import java.util.Stack;
 
 /**
  * Created by ahmed on 11/19/17.
+ * <p>
+ * Manages sending data to GPU
  */
 
 class ShaderDataLoader {
     private Stack<Integer> handleStack;
     private States state;
+
+    /**
+     * How many bytes per float.
+     */
+    private static final int FLOAT_SIZE = 4;
 
     private enum States {
         Started,
@@ -23,6 +30,9 @@ class ShaderDataLoader {
         state = States.Closed;
     }
 
+    /**
+     * Put the machine in the start state
+     */
     void start() {
         assertInState(States.Closed);
 
@@ -30,15 +40,26 @@ class ShaderDataLoader {
             throw new RuntimeException("Attribute pointer wasn't closed, call disableHandles() first");
         }
 
-        this.state = States.Started;
+        state = States.Started;
     }
 
-    void loadData(int handle, int length,
-                  int type, boolean normalized, int strideInBytes, Buffer buffer) {
+    /**
+     * Load data to shaders
+     *
+     * @param handle              handle of the shader variable
+     * @param itemLength          number of items in an instance i.e 3 floats/location point
+     * @param type                GL_FLOAT...atc
+     * @param normalized          Determines whether the valeus are norm
+     * @param strideInBytes       Number of bytes to jump when reading to get a value
+     * @param singleContentBuffer Buffer containing data
+     */
+    void loadData(int handle, int itemLength,
+                  int type, boolean normalized, int strideInBytes, Buffer singleContentBuffer) {
         assertInState(States.Started);
 
-        GLES20.glVertexAttribPointer(handle, length, type, normalized,
-                strideInBytes, buffer);
+        singleContentBuffer.position(0);
+        GLES20.glVertexAttribPointer(handle, itemLength, type, normalized,
+                strideInBytes, singleContentBuffer);
         ErrorChecker.checkGlError("LOAD_DATA:glVertexAttribPointer");
 
         GLES20.glEnableVertexAttribArray(handle);
@@ -47,6 +68,9 @@ class ShaderDataLoader {
         handleStack.push(handle);
     }
 
+    /**
+     * Handles to data should be disabled after drawing
+     */
     void disableHandles() {
         assertInState(States.Started);
 
@@ -54,7 +78,7 @@ class ShaderDataLoader {
             GLES20.glDisableVertexAttribArray(handleStack.pop());
         }
 
-        this.state = States.Closed;
+        state = States.Closed;
     }
 
     private void assertInState(States expected) {
