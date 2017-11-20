bf89b622708e82573e0b4599b961de960bb9ca9a
shakram02 <ahmedhamdyau@gmail.com>
2017-11-20 11:20:41 +0200

    GLProgram abstraction

--- app/src/main/java/com/example/ahmed/opengl2lesson/graphics/BasicRenderer.java
+++ app/src/main/java/com/example/ahmed/opengl2lesson/graphics/BasicRenderer.java
@@ -8,7 +8,7 @@ import android.os.SystemClock;
 import com.example.ahmed.opengl2lesson.graphics.gl_internals.memory.ColorArray;
 import com.example.ahmed.opengl2lesson.graphics.gl_internals.FrustumManager;
 import com.example.ahmed.opengl2lesson.graphics.gl_internals.ShaderDataLoader;
-import com.example.ahmed.opengl2lesson.graphics.gl_internals.ShaderManager;
+import com.example.ahmed.opengl2lesson.graphics.gl_internals.memory.GLProgram;
 import com.example.ahmed.opengl2lesson.graphics.gl_internals.memory.VertexArray;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -27,47 +27,6 @@ public class BasicRenderer implements GLSurfaceView.Renderer {
      */
     private ColorArray mTriangleColors;
 
-    private final String vertexShader =
-            // A constant representing the combined model/view/projection matrix.
-            "uniform mat4 u_MVPMatrix;      \n"
-                    // Per-vertex position information we will pass in.
-                    + "attribute vec4 a_Position;     \n"
-
-                    // Per-vertex color information we will pass in.
-                    + "attribute vec4 a_Color;        \n"
-
-                    // This will be passed into the fragment shader.
-                    + "varying vec4 v_Color;          \n"
-
-                    + "void main()                    \n"
-                    + "{                              \n"
-
-                    // Pass the color through to the fragment shader.
-                    + "   v_Color = a_Color;          \n"
-
-                    // gl_Position is a special variable used to store the final position.
-                    // Multiply the vertex by the matrix to get the final point in
-                    // normalized screen coordinates.
-                    + "   gl_Position = u_MVPMatrix   \n"
-                    + "               * a_Position;   \n"
-                    + "}                              \n";
-
-    private final String fragmentShader =
-            // Set the default precision to medium. We don't need as
-            // high of a precision in the fragment shader.
-            "precision mediump float;       \n"
-                    // This is the color from the vertex shader interpolated across the
-                    + "varying vec4 v_Color;          \n"
-                    // triangle per fragment.
-                    + "void main()                    \n"
-                    + "{                              \n"
-                    + "   gl_FragColor = v_Color;     \n"
-                    + "}                              \n";
-
-    /**
-     * How many bytes per float.
-     */
-    private final int mBytesPerFloat = 4;
     private VertexArray mTriangleVertices;
 
     /**
@@ -75,21 +34,6 @@ public class BasicRenderer implements GLSurfaceView.Renderer {
      * it positions things relative to our eye.
      */
     private float[] mViewMatrix = new float[16];
-
-    /**
-     * This will be used to pass in the transformation matrix.
-     */
-    private int mMVPMatrixHandle;
-
-    /**
-     * This will be used to pass in model position information.
-     */
-    private int mPositionHandle;
-
-    /**
-     * This will be used to pass in model color information.
-     */
-    private int mColorHandle;
 
     /**
      * Store the projection matrix. This is used to project the scene onto a 2D viewport.
@@ -107,6 +51,8 @@ public class BasicRenderer implements GLSurfaceView.Renderer {
 
     }
 
+    private GLProgram program;
+    private String mvpMatrixVariableName = "u_MVPMatrix";
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -131,19 +77,46 @@ public class BasicRenderer implements GLSurfaceView.Renderer {
         Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX,
                 lookY, lookZ, upX, upY, upZ);
 
-        int vertexShaderHandle = ShaderManager.compileVertexShader(vertexShader);
-        int fragmentShaderHandle = ShaderManager.compileFragmentShader(fragmentShader);
+        String fragmentShader = "precision mediump float;       \n"
+                // This is the color from the vertex shader interpolated across the
+                + "varying vec4 v_Color;          \n"
+                // triangle per fragment.
+                + "void main()                    \n"
+                + "{                              \n"
+                + "   gl_FragColor = v_Color;     \n"
+                + "}                              \n";
+        String vertexShader = "uniform mat4 u_MVPMatrix;      \n"
+                // Per-vertex position information we will pass in.
+                + "attribute vec4 a_Position;     \n"
 
-        // Create a program object and store the handle to it.
-        int programHandle = ShaderManager.createProgram(vertexShaderHandle, fragmentShaderHandle);
+                // Per-vertex color information we will pass in.
+                + "attribute vec4 a_Color;        \n"
 
-        // Set program handles. These will later be used to pass in values to the program.
-        mMVPMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
-        mPositionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
-        mColorHandle = GLES20.glGetAttribLocation(programHandle, "a_Color");
+                // This will be passed into the fragment shader.
+                + "varying vec4 v_Color;          \n"
+
+                + "void main()                    \n"
+                + "{                              \n"
+
+                // Pass the color through to the fragment shader.
+                + "   v_Color = a_Color;          \n"
+
+                // gl_Position is a special variable used to store the final position.
+                // Multiply the vertex by the matrix to get the final point in
+                // normalized screen coordinates.
+                + "   gl_Position = u_MVPMatrix   \n"
+                + "               * a_Position;   \n"
+                + "}                              \n";
+
+        program = new GLProgram(vertexShader, fragmentShader);
+        String positionVariableName = "a_Position";
+        program.declareAttribute(positionVariableName);
+        String colorVariableName = "a_Color";
+        program.declareAttribute(colorVariableName);
+        program.declareUniform(mvpMatrixVariableName);
 
         // Tell OpenGL to use this program when rendering.
-        GLES20.glUseProgram(programHandle);
+        program.activate();
 
         // This triangle is red, green, and blue.
         final float[] triangle1VerticesData = {
@@ -160,8 +133,10 @@ public class BasicRenderer implements GLSurfaceView.Renderer {
                 0.0f, 1.0f, 0.0f, 1.0f
         };
 
-        mTriangleVertices = new VertexArray(triangle1VerticesData, mPositionHandle, true);
-        mTriangleColors = new ColorArray(triangleColorData, mColorHandle, true);
+        mTriangleVertices = new VertexArray(triangle1VerticesData,
+                program.getVariableHandle(positionVariableName), true);
+        mTriangleColors = new ColorArray(triangleColorData,
+                program.getVariableHandle(colorVariableName), true);
     }
 
     @Override
@@ -194,11 +169,7 @@ public class BasicRenderer implements GLSurfaceView.Renderer {
 
     private void drawTriangle() {
         shaderDataLoader.start();
-
-        // Pass in the position information
         shaderDataLoader.loadData(mTriangleVertices);
-
-        // Pass in the color information
         shaderDataLoader.loadData(mTriangleColors);
 
         // This multiplies the view matrix by the model matrix, and stores the
@@ -209,7 +180,8 @@ public class BasicRenderer implements GLSurfaceView.Renderer {
         // (which now contains model * view * projection).
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
 
-        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+        GLES20.glUniformMatrix4fv(program.getVariableHandle(mvpMatrixVariableName),
+                1, false, mMVPMatrix, 0);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
 
         shaderDataLoader.disableHandles();
