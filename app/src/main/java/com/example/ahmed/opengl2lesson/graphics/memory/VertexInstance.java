a4b794710a283c4e05188eaf01dd489d0f904e5b
shakram02 <ahmedhamdyau@gmail.com>
2017-11-20 10:11:37 +0200

    create instnace array from float[]

--- app/src/main/java/com/example/ahmed/opengl2lesson/graphics/memory/VertexInstance.java
+++ app/src/main/java/com/example/ahmed/opengl2lesson/graphics/memory/VertexInstance.java
@@ -5,6 +5,7 @@ package com.example.ahmed.opengl2lesson.graphics.memory;
  */
 
 class VertexInstance {
+    private static final int FLOATS_PER_INSTANCE = 3;
     final float x;
     final float y;
     final float z;
@@ -17,5 +18,16 @@ class VertexInstance {
 
     float[] array() {
         return new float[]{x, y, z};
+    }
+
+    static VertexInstance[] fromFloats(float[] array) {
+        int vertexCount = array.length / FLOATS_PER_INSTANCE;
+        VertexInstance[] packed = new VertexInstance[vertexCount];
+
+        for (int i = 0, j = 0; j < vertexCount; i += 3, j++) {
+            packed[j] = new VertexInstance(array[i], array[i + 1], array[i + 2]);
+        }
+
+        return packed;
     }
 }
